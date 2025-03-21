5090b237aa1aa65988622f126e1e9e0ada4fd249
Alimirzaei84 <mirzaeiali4000@gmail.com>
2025-03-22 01:42:28 +0330

    create home page

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',RedirectView.as_view(url='/catalog/', permanent=True)),
+    path('catalog/', include('catalog.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
